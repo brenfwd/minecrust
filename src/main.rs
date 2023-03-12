a5baf4e6a88df802c52fac6da659035a4a2791f6
Brenden Forward <bforward@umich.edu>
2023-03-12 00:59:13 +0000

    Add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 use async_std::{
-    io::{ReadExt, WriteExt},
+    io::{self, ReadExt, WriteExt},
     net::{TcpListener, TcpStream},
     task,
 };
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum BufferError {
+    #[error("out of bounds (want {want} bytes, remaining {remaining})")]
+    OutOfBounds { want: usize, remaining: usize },
+    #[error("invalid varint position ({position} > {limit})")]
+    InvalidVarIntSize { position: usize, limit: usize },
+    #[error("invalid string length ({0})")]
+    InvalidStringLength(i32),
+    #[error("invalid UTF-8 string")]
+    InvalidUTF8String,
+}
+
+type BufferResult<T> = Result<T, BufferError>;
 
 struct Buffer {
     data: Vec<u8>,
@@ -31,18 +47,27 @@ impl Buffer {
         self
     }
 
-    pub fn read_u8(&mut self) -> u8 {
-        assert!(self.remaining() > 0);
-        self.data.remove(0)
+    fn check_bytes(&mut self, want: usize) -> BufferResult<()> {
+        let remaining = self.remaining();
+        if remaining < want {
+            Err(BufferError::OutOfBounds { want, remaining })
+        } else {
+            Ok(())
+        }
     }
 
-    pub fn read_var_int(&mut self) -> i32 {
+    pub fn read_u8(&mut self) -> BufferResult<u8> {
+        self.check_bytes(1)?;
+        Ok(self.data.remove(0))
+    }
+
+    pub fn read_var_int(&mut self) -> BufferResult<i32> {
         let mut value: i32 = 0;
         let mut position = 0;
         let mut current_byte;
 
         loop {
-            current_byte = self.read_u8();
+            current_byte = self.read_u8()?;
             // TODO: see if this works with signed/unsigned numbers properly
             value |= ((current_byte & 0b01111111) as i32) << position; // last 7 bits
             if (current_byte & 0b10000000) == 0 {
@@ -50,40 +75,63 @@ impl Buffer {
                 break;
             }
             position += 7;
-            assert!(position < 32);
+            if position >= 32 {
+                return Err(BufferError::InvalidVarIntSize {
+                    position,
+                    limit: 32,
+                });
+            }
         }
 
-        value
+        Ok(value)
     }
 
-    pub fn read_bytes(&mut self, into: &mut [u8]) {
+    pub fn read_bytes(&mut self, into: &mut [u8]) -> BufferResult<()> {
         let len = into.len();
-        assert!(self.remaining() >= len);
+        self.check_bytes(len)?;
         for i in 0..len {
-            into[i] = self.read_u8();
+            into[i] = self.read_u8()?;
+        }
+        Ok(())
+    }
+
+    pub fn read_buffer(&mut self, length: usize) -> BufferResult<Buffer> {
+        let mut bytes = vec![0; length];
+        self.read_bytes(&mut bytes)?;
+        Ok(Buffer::from_vec(bytes))
+    }
+
+    pub fn read_string(&mut self) -> BufferResult<String> {
+        let length = self.read_var_int()?;
+        if length < 0 {
+            return Err(BufferError::InvalidStringLength(length));
+        }
+        let mut bytes = vec![0; length as usize];
+        self.read_bytes(&mut bytes)?;
+        match String::from_utf8(bytes) {
+            Ok(str) => Ok(str),
+            Err(e) => Err(BufferError::InvalidUTF8String),
         }
     }
 
-    pub fn read_buffer(&mut self, length: usize) -> Buffer {
-        let mut bytes = vec![0; length];
-        self.read_bytes(&mut bytes);
-        Buffer::from_vec(bytes)
-    }
-
-    pub fn read_string(&mut self) -> String {
-        let length = self.read_var_int();
-        assert!(length >= 0);
-        let mut bytes = vec![0; length as usize];
-        self.read_bytes(&mut bytes);
-        String::from_utf8(bytes).unwrap()
-    }
-
-    pub fn read_ushort(&mut self) -> u16 {
+    pub fn read_ushort(&mut self) -> BufferResult<u16> {
         let mut bytes = [0; 2];
-        self.read_bytes(&mut bytes);
-        u16::from_be_bytes(bytes)
+        self.read_bytes(&mut bytes)?;
+        Ok(u16::from_be_bytes(bytes))
     }
 }
+
+#[derive(Debug, Error)]
+enum ClientError {
+    #[error("packet decode error: {0:?}")]
+    PacketDecodeError(#[from] BufferError),
+    #[error("logic error: ({0})")]
+    LogicError(String),
+    #[error("network error: {0:?}")]
+    NetworkError(#[from] io::Error),
+}
+
+type ClientResult<T> = Result<T, ClientError>;
 
 #[derive(Debug)]
 enum ClientState {
@@ -108,22 +156,14 @@ impl Client {
         }
     }
 
-    pub async fn handle(&mut self) {
+    pub async fn handle(&mut self) -> ClientResult<()> {
         let mut frame = [0; 1024];
         loop {
-            let bytes_read = match self.stream.read(&mut frame).await {
-                Ok(n) => {
-                    if n == 0 {
-                        println!("\tEnd of stream!");
-                        return;
-                    }
-                    n
-                }
-                Err(e) => {
-                    println!("\tError: {e}");
-                    return;
-                }
-            };
+            let bytes_read = self.stream.read(&mut frame).await?;
+            if bytes_read == 0 {
+                println!("End of stream!");
+                return Ok(());
+            }
             // append frame bytes into self.buf:
             self.buf.push_slice(&frame[..bytes_read]);
             println!(
@@ -133,7 +173,7 @@ impl Client {
             );
 
             // handle the packets
-            self.process_packets();
+            self.process_packets()?;
         }
     }
 
@@ -141,39 +181,43 @@ impl Client {
         self.stream.peer_addr().unwrap().to_string()
     }
 
-    fn process_packets(&mut self) {
+    fn process_packets(&mut self) -> ClientResult<()> {
         // Each packet:
         //      Length      VarInt
         //      PacketID    VarInt
         //      Data        Bytes...
 
-        let length = self.buf.read_var_int();
+        let length = self.buf.read_var_int()?;
         assert!(length >= 0);
 
-        let mut payload = self.buf.read_buffer(length as usize);
+        let mut payload = self.buf.read_buffer(length as usize)?;
 
-        let packet_id = payload.read_var_int();
+        let packet_id = payload.read_var_int()?;
 
         dbg!(packet_id);
 
         match (&self.state, packet_id) {
             (ClientState::Handshaking, 0x00) => {
-                let protocol_version = payload.read_var_int();
-                let server_address = payload.read_string();
-                let server_port = payload.read_ushort();
-                let next_state = payload.read_var_int();
+                let protocol_version = payload.read_var_int()?;
+                let server_address = payload.read_string()?;
+                let server_port = payload.read_ushort()?;
+                let next_state = payload.read_var_int()?;
                 dbg!(protocol_version, server_address, server_port, next_state);
                 match next_state {
                     1 => self.state = ClientState::Status,
                     2 => self.state = ClientState::Login,
                     _ => {
-                        eprintln!("Invalid next_state: {next_state}");
-                        return;
+                        return Err(ClientError::LogicError(format!(
+                            "Invalid next_state: {next_state} (from {:?}",
+                            self.state
+                        )));
                     }
                 }
+                return Ok(());
             }
             _ => {
                 eprintln!("\tUnhandled packet ID: {:?}/{packet_id}", &self.state);
+                return Ok(());
             }
         }
     }
@@ -183,7 +227,16 @@ async fn handle_client(stream: TcpStream) {
     let mut client = Client::new(stream);
     println!("New client connected: {}", client.get_addr());
 
-    client.handle().await;
+    match client.handle().await {
+        Err(error) => {
+            eprintln!(
+                "An error occurred in the client connection for {}: {:?}",
+                client.get_addr(),
+                error
+            );
+        }
+        _ => {}
+    }
 
     println!("Client going out of scope: {}\n", client.get_addr());
 }
